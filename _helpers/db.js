bb8ec753dbdbdaeaf017c729f83c0484dc216ca8
Retno Sonjaya <beno.sons@khansia.co.id>
2022-07-14 17:28:30 +0700

    supabase

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,6 +1,7 @@
 const config = require('config.json');
-const mysql = require('mysql2/promise');
+// const mysql = require('mysql2/promise');
 const { Sequelize } = require('sequelize');
+const Pool = require('pg').Pool
 
 module.exports = db = {};
 
@@ -9,11 +10,19 @@ initialize();
 async function initialize() {
     // create db if it doesn't already exist
     const { host, port, user, password, database } = config.database;
-    const connection = await mysql.createConnection({ host, port, user, password });
-    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+    const connection = new Pool({
+        user: user,
+        host: host,
+        database: database,
+        password: password,
+        port: port,
+      })
+    // const connection = await mysql.createConnection({ host, port, user, password });
+    // await connection.query(`SELECT 'CREATE DATABASE mydb'
+    // WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${database}');`);
 
     // connect to db
-    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+    const sequelize = new Sequelize(database, user, password, { dialect: 'postgres' });
 
     // init models and add them to the exported db object
     db.User = require('../users/user.model')(sequelize);
